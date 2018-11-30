e349ed591dcc699f21b311355ec9971f6e67a4fb
Maria Campana <maria.campana@beamery.com>
2018-11-30 12:41:44 +0000

    Add not used main package

---
+++ notused/go.mod
@@ -0,0 +1,3 @@
+module gitlab.com/maria.campana/gologger/notused
+
+require gitlab.com/maria.campana/gologger/logger v0.1.1
---
+++ notused/notused.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"gitlab.com/maria.campana/gologger/logger"
+)
+
+func main() {
+	logger.Info("This should not be used")
+}
